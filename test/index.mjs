4f69089af4fd4cecfe7509ef8d60524447a574a5
othree <othree@gmail.com>
2018-04-09 14:03:26 +0800

    Add tests

--- test/index.mjs
+++ test/index.mjs
@@ -1,12 +1,33 @@
 import test from 'tape';
 import query from '../';
 
-test('timing test', function (t) {
+test('Basic Test', function (t) {
   t.plan(3);
 
-  const q = query('HTML or XHTML');
+  const q = query('XML or XHTML');
 
-  t.ok(q('HTML'));
+  t.notOk(q('HTML'));
   t.ok(q('XHTML'));
-  t.notOk(q('XML'));
+  t.ok(q('XML'));
 });
+
+test('Has Matcher', function (t) {
+  t.plan(3);
+
+  const q = query('XML or HTML');
+
+  t.ok(q('HTML', {matcher: 'has'}));
+  t.ok(q('XHTML', {matcher: 'has'}));
+  t.ok(q('XML', {matcher: 'has'}));
+});
+
+test('Eqeq Matcher', function (t) {
+  t.plan(3);
+
+  const q = query('XML or HTML');
+
+  t.ok(q('HTML', {matcher: 'eqeq'}));
+  t.notOk(q('XHTML', {matcher: 'eqeq'}));
+  t.ok(q('XML', {matcher: 'eqeq'}));
+});
+
